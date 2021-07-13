f347ea46ee62f99e226fe458d09a16e22de9ff8b
Marcus <marconiushenry@gmail.com>
2021-07-13 14:52:07 -0500

    - correct spelliing erroor in product review form

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -183,7 +183,7 @@ export default function ProductScreen(props) {
                         <option value="2">2- Fair</option>
                         <option value="3">3- Good</option>
                         <option value="4">4- Very Good</option>
-                        <option value="5">5- Excelent</option>
+                        <option value="5">5- Excellent</option>
                       </select>
                     </div>
                     <div>
